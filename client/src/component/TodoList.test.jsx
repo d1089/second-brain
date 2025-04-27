e25fa34ed0714b6da219de0680c2f3dc5fda1943
d1089 <dipak.prajapati1729@gmail.com>
2025-04-27 12:41:36 +0530

    ts

--- client/src/component/TodoList.test.jsx
+++ client/src/component/TodoList.test.jsx
@@ -1,6 +1,8 @@
 // src/TodoApp.test.jsx
 import { render, screen, fireEvent } from "@testing-library/react";
 import { TodoList } from "./TodoList";
+import "@testing-library/jest-dom";
+import { describe, it, expect } from "vitest";
 
 describe("TodoList", () => {
   it("should allow the user to add a new todo", () => {
